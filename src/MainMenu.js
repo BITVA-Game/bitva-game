e6b36c43fb069ce71626515852dace3c834d09e5
Inga Pflaumer <inga.pflaumer@gmail.com>
2018-07-29 21:02:56 +1000

    Added images to main page

--- src/MainMenu.js
+++ src/MainMenu.js
@@ -1,13 +1,48 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './App.css';
+import chars from './images/IconCharacters.png';
+import cards from './images/IconCards.png';
+import silver from './images/IconMoney.png';
+import gold from './images/IconSpecial.png';
+import ornament from './images/MainOrnament.jpg'
 
-const MainMenu = props => (
+
+const TopButton = (props) =>(	
+	<div className='TopButton'>
+		<img className='TopIcon' src={props.icon} /> 
+		<button type='button' onClick={() => props.sendMessage({ type: props.type })}> 
+			{props.text} 
+		</button>
+	</div>
+);
+
+const MainMenu = (props) => (
     <div>
-        <textarea value={JSON.stringify(props.app.profile)} />
-        <button type="button" onClick={() => props.sendMessage({ type: 'PLAY' })}>
-Select imaginary PvP
-        </button>
+    	<div className='TopShelf'>
+	    	<TopButton icon={chars} sendMessage={props.sendMessage} type="PROPFILECHARACTERS" text="Characters" />
+	    	<TopButton icon={cards} sendMessage={props.sendMessage} type="PROPFILECARDS" text="Cards" />
+	    	<TopButton icon={silver} sendMessage={props.sendMessage} type="SHOP" text="Coins" />
+	    	<TopButton icon={gold} sendMessage={props.sendMessage} type="SHOP" text="Shards" />
+	    </div>
+        {/*<textarea value={JSON.stringify(props.app.profile)} />*/}
+        <div class='Content'>
+        	<img className='MainOrnament' src={ornament}/>
+	        <ul>
+	        	<li>
+	        		<button className='MenuButton' type="button" onClick={() => props.sendMessage({ type: 'PROFILE' })}> Profile</button>
+	        	</li>
+	        	<li>
+	        		<button className='MenuButton' type="button" onClick={() => props.sendMessage({ type: 'SETTINGS' })}> Settings</button>
+	        	</li>
+	        	<li>
+	        		<button className='MenuButton' type="button" onClick={() => props.sendMessage({ type: 'PLAY' })}> Play PvP</button>
+	        	</li>
+	        	<li>	
+	        		<button className='MenuButton' type="button" onClick={() => props.sendMessage({ type: 'PLAY' })}> Play Story</button>
+	        	</li>
+	        </ul>
+	     </div>
     </div>
 );
 
