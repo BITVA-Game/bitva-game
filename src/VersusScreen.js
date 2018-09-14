ef2d67a7c0110bb7c232a2382d65821f8aca40ac
ML6503 <ml6503@gmail.com>
2018-09-14 17:10:06 +0300

    Versus tkt part

--- src/VersusScreen.js
+++ src/VersusScreen.js
@@ -1,15 +1,38 @@
 import React from 'react';
 // import PropTypes from 'prop-types';
 import './css/App.css';
+import './css/Versus.css';
 
+import yaga from './images/heroes/yaga.jpg';
+import morevna from './images/heroes/morevna.jpg';
+
+const images = {
+    yaga,
+    morevna,
+};
+
+const renderHeroes = function (players){
+	players.forEach(function(p){
+		<OneHero hero={p.hero}/>
+	})
+}
+
+const OneHero = props => (
+    <div className="OneHero">
+        <img src={images[props.hero]} alt={props.hero} />
+        </div>
+);
 
 const VersusScreen = props => (
     <div>
         <h3>
             Versus Screen
-            {console.log(props)}
-        </h3>
-    </div>
+         </h3>
+            <OneHero hero={props.app.game.players[0].hero}/>
+            <OneHero hero={props.app.game.players[1].hero}/>
+            
+        
+    </div> 
 );
 
 
