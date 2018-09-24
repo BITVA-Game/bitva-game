9287e358903e841ec3b754893c03957a28dd6e34
ML6503 <ml6503@gmail.com>
2018-09-24 22:23:38 +0300

    key ID added to each player

--- backend/gameEngine.js
+++ backend/gameEngine.js
@@ -15,53 +15,24 @@ function getRandomBool() {
     return rand === 0;
 }
 
-String.prototype.hashCode = function() {
-  var hash = 0, i, chr;
-  if (this.length === 0) return hash;
-  for (i = 0; i < this.length; i++) {
-    chr   = this.charCodeAt(i);
-    hash  = ((hash << 5) - hash) + chr;
-    hash |= 0; // Convert to 32bit integer
-  }
-  return hash;
-}
-
-function getCardID(id) {
-  var now = new Date();
-  /*id = now.getFullYear().toString(); 
-  // JS months are 0-based, so +1 and pad with 0's
-  id += (now.getFullMonth < 9 ? '0' : '') + now.getFullMonth().toString(); 
-  id += (now.getDate < 10 ? '0' : '') + now.getDate().toString();
-  id = now.getHours().toString();
-  id += now.getMinutes().toString();
-  id += now.getSeconds().toString();
-  id += now.getMilliseconds().toString(); */
-  id += '-' + id.hashCode(id + now.toString());
-  
-  return id;
-}
-
 function assignCards(heroName) {
-// const cards = {};
 // find all cardTypes for this heroName
     const heroCardTypes = allCharacters[heroName].cards;
     // for each cardType in all CardTypes take count
     const cardsArray = [];
-
     for (const cardType in heroCardTypes) {
         // take count, create a new card for this type
         for (let i = 0; i < heroCardTypes[cardType].count; i += 1) {
             const newCard = allCards[cardType];
-            // cards[newCard.id] = newCard;
             cardsArray.push(newCard);
         }
     }
     const deck = cardsArray.sort(() => Math.random() - 0.5);
     const deckHero = deck.slice(0, 15);
-    deckHero.forEach((c) => {
-        c.id = getCardID(c.id);
-    });
-    console.log(deckHero.length);
+    for (let j = 0; j < deckHero.length; j += 1) {
+        const keyId = `key ${j}`;
+        deckHero[j] = keyId;
+    }
     return deckHero;
 }
 
@@ -109,7 +80,7 @@ const generatePlayers = function (heroName) {
 function giveCardsTo(player) {
     const x = player.playerHand.length >= 0 ? 5 - player.playerHand.length : 5;
     player.playerHand = player.cards.splice(0, x);
-    
+
     return player;
 }
 
