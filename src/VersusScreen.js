00a564e926c65f4a552bd1ec02b3f55397293008
MentalWombat <e.ochneva@gmail.com>
2018-09-29 17:17:48 +0300

    fixed VersusScreen

--- src/VersusScreen.js
+++ src/VersusScreen.js
@@ -18,42 +18,26 @@ const images = {
 }; */
 
 const OneHero = props => (
-    <div className="OneHeroVersus">
+    <div className="versus-one-hero">
         <img src={images[props.hero]} alt={props.hero} />
     </div>
 );
 
 const VersusScreen = props => (
-    <div className="VersusScreenDiv">
-        <div className="TitleDiv">
-            <h3 className="Title">
-            Versus Screen
-            </h3>
-        </div>
-        <div className="containerVersus">
-            <div>
-
-                <OneHero hero={props.app.game.players[0].hero} />
-
-            </div>
+    <div className="versus-screen-container">
+        <div className="versus-heroes-container">
+            <OneHero hero={props.app.game.players[0].hero} />
             <div className="vs">
                 <p>
                 VS
                 </p>
             </div>
-            <div>
-
-                <OneHero hero={props.app.game.players[1].hero} />
-
-            </div>
-
+            <OneHero hero={props.app.game.players[1].hero} />
         </div>
-        <div className="PlayButtonLayer">
-            <div className="ButtonDiv">
-                <button className="PlayButton" type="button" onClick={() => props.sendMessage({ type: 'DEALALL' })}>
-                PLAY
-                </button>
-            </div>
+        <div className="play-button-container">
+            <button className="play-button" type="button" onClick={() => props.sendMessage({ type: 'DEALALL' })}>
+            PLAY
+            </button>
         </div>
     </div>
 );
