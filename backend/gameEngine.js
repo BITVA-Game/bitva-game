d39aacafaa52c665562df2989c5f95463b872f8e
ML6503 <ml6503@gmail.com>
2018-10-14 14:57:47 +0300

    case1 corrected

--- backend/gameEngine.js
+++ backend/gameEngine.js
@@ -69,6 +69,7 @@ const generatePlayers = function (heroName) {
             p.cards = assignCards(p.deck);
             p.hand = {};
             p.grave = {};
+            p.moveCounter = 0;
             p.health = allCharacters[heroName].health;
         }
         if (p.active === false) {
@@ -77,6 +78,7 @@ const generatePlayers = function (heroName) {
             p.cards = assignCards(p.deck);
             p.hand = {};
             p.grave = {};
+            p.moveCounter = 0;
             p.health = allCharacters[heroSecondName].health;
         }
     });
@@ -90,6 +92,7 @@ function randomKey(hashtable) {
 }
 
 function giveCardsTo(player) {
+    //console.log(Object.keys(player.hand));
     while (Object.keys(player.hand).length < 5) {
         const key = randomKey(player.cards);
         player.hand[key] = player.cards[key];
@@ -107,6 +110,25 @@ function giveCardsToAll(playersArray) {
     return playersArray;
 }
 
+function increaseCounter(player) {
+    player.moveCounter += 1;
+}
+
+function makeMove(game, msg) {
+    game.players.forEach((p) => {
+        if (p.active) {
+            p.hand.forEach((c) => {
+                if (c.key === msg.key && msg.category === 'graveyard') {
+                    p.grave = p.hand.splice(c, 1);
+                }
+            });
+            increaseCounter(p);
+        }
+    });
+    return game;
+ }
+
+
 function handle(app, message) {
     switch (message.type) {
     case 'INITIAL': {
@@ -121,6 +143,10 @@ function handle(app, message) {
         const playersArray = app.game.players;
 
         return Object.assign({}, app.game, giveCardsToAll(playersArray));
+    }
+    case 'CASE1': {
+
+        return Object.assign({}, app.game, makeMove(app.game, message));
     }
     default: { return app.game; }
     }
--- backend/screenManager.js
+++ backend/screenManager.js
@@ -10,6 +10,8 @@ function handle(app, message) {
         return Object.assign({}, app.manager, { screen: 'PLAYERACT' });
     case 'STARTSCREEN':
         return Object.assign({}, app.manager, { screen: 'STARTSCREEN' });
+    case 'CASE1':
+        return Object.assign({}, app.manager, { screen: 'GAMESCREEN' });
     default: return app.manager;
     }
 }
