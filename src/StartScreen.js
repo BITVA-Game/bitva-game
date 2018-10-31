79e250faadf04aa1bec074512893537001849c2a
almaceleste <pcaptanovska@gmail.com>
2018-10-31 20:36:33 +0300

    added window animation

--- src/StartScreen.js
+++ src/StartScreen.js
@@ -9,6 +9,8 @@ import './css/StartScreen.css';
 const StartScreen = props => (
     <div className="start-screen">
         <div className="hollow" />
+        <div className="window window-left" />
+        <div className="window window-right" />
         <MainMenu sendMessage={props.sendMessage} opened />
         {/* <textarea value={JSON.stringify(props.app.profile)} /> */}
     </div>
