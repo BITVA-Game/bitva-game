1ab8645bc93f92aa2c8f10a7cea35757f6f972b9
Inga Pflaumer <inga.pflaumer@gmail.com>
2018-11-11 17:51:01 +1100

    Message for the card and graveyard + functionality

--- backend/screenManager.js
+++ backend/screenManager.js
@@ -10,17 +10,9 @@ function handle(app, message) {
         return Object.assign({}, app.manager, { screen: 'PLAYERACT' });
     case 'STARTSCREEN':
         return Object.assign({}, app.manager, { screen: 'STARTSCREEN' });
-    case 'CASE1':
+    case 'ACTION':
         return Object.assign({}, app.manager, { screen: 'GAMESCREEN' });
-    case 'CASE2':
-        return Object.assign({}, app.manager, { screen: 'GAMESCREEN' });
-    case 'CASE3':
-        return Object.assign({}, app.manager, { screen: 'GAMESCREEN' });
-<<<<<<< HEAD
-=======
-    case 'CASE4':
-        return Object.assign({}, app.manager, { screen: 'GAMESCREEN' });
->>>>>>> b3935c146013abc7f59a14a418926b1a4ac0781b
+
     default: return app.manager;
     }
 }
