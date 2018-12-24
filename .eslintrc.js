84f4ba23b769223e9f3d0f3ef12149a461de5e0c
Inga Pflaumer <inga.pflaumer@gmail.com>
2018-12-24 23:34:52 +1100

    Relaxed the rule for no tab-index

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,13 +8,14 @@ module.exports = {
     	"indent": ["error", 4],
     	"no-console": [0],
     	"react/jsx-indent": ["error", 4],
-        "react/jsx-indent-props": ["error", 4],
-        "react/jsx-filename-extension": [0],
-        "prefer-destructuring": [0],
-        "react/destructuring-assignment": [0],
-        "react/forbid-prop-types": [0],
-        "guard-for-in": [0],
-        "no-restricted-syntax": [0]
+      "react/jsx-indent-props": ["error", 4],
+      "react/jsx-filename-extension": [0],
+      "prefer-destructuring": [0],
+      "react/destructuring-assignment": [0],
+      "react/forbid-prop-types": [0],
+      "guard-for-in": [0],
+      "no-restricted-syntax": [0],
+			"tabindex-no-positive": [0]
     },
     "env": {
     	"browser": true,
