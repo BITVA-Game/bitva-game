464458962933f4bc97c8b3c5669077ddb4fd44cf
MentalWombat <e.ochneva@gmail.com>
2018-12-27 23:10:14 +0300

    getting cards

--- backend/heroSelect.js
+++ backend/heroSelect.js
@@ -1,10 +1,31 @@
 const heroes = require('./data/characters.json');
+const cards = require('./data/cards.json');
+
+function getCards(heroes) {
+	const heroArr = Object.values(heroes);
+	const cardsArr = Object.keys(cards);
+	heroArr.forEach((h) => {
+		for(let i = 0; i < heroArr.length; i += 1) {
+			const heroCards = Object.keys(heroArr[i]['cards']);
+			console.log(heroCards);
+			for(let j = 0; j < heroCards.length;  j += 1) {
+				console.log(heroCards[j], cardsArr[j]);
+				if(heroCards[j] == cardsArr[j]) {
+					heroCards[j] = cardsArr[j];
+				}
+			}
+		}
+		console.log(heroCards);
+		return heroArr;
+	});
+}
 
 function handle(app, message) {
     switch (message.type) {
     case 'INITIAL':
         return app.heroSelect;
     case 'PLAY':
+    	console.log(getCards(heroes));
         return Object.assign({}, app.heroSelect, heroes);
     case 'HEROSELECTED':
         return Object.assign({});
