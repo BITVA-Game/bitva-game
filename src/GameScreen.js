f0a77e9fa9f373a526f415172219fa0124fe164e
Ekaterina Davliatchina <flykat@yandex.ru>
2019-02-21 00:48:23 +1000

    work in progress

--- src/GameScreen.js
+++ src/GameScreen.js
@@ -8,7 +8,7 @@ import './css/GameScreen.css';
 class GameScreen extends Component {
     constructor(props) {
         super(props);
-        this.state = { dragging: null };
+        this.state = { dragging: null, active: true };
         this.cardDragStarted = this.cardDragStarted.bind(this);
         this.cardDropped = this.cardDropped.bind(this);
         this.cardDragEnded = this.cardDragEnded.bind(this);
@@ -35,8 +35,15 @@ class GameScreen extends Component {
         });
     }
 
+    changePlayer() {
+        this.setState({
+            active: null,
+        });
+    }
+
     render() {
         console.log('I AM RENDERING!');
+        console.log(this.props.changePlayer);
         return (
             <div className="game-table app-background">
                 {this.props.app.game.players.map(player => (
@@ -52,6 +59,14 @@ class GameScreen extends Component {
                         cardDragEnded={this.cardDragEnded}
                     />
                 ))}
+                {this.state.active === true
+                    ? (
+                        <ChangeTurn
+                            changePlayer={this.changePlayer}
+                            players={this.props.app.game.players}
+                        />
+                    )
+                    : null}
                 {this.props.app.game.phase === 'OVER'
                     ? (
                         <GameOver
@@ -76,6 +91,24 @@ const GameOver = (props) => {
         </div>
     );
 };
+
+const ChangeTurn = (props) => {
+    /*let players = props.players;
+    for (const p of players) {
+        if (p.active === true) {
+           props.changePlayer();
+        }
+    }*/
+    let activePlayer = props.players[0];
+    if (props.players[0].active === false) {
+        activePlayer = props.players[1];
+    }
+    return (
+        <div className="changeturn">
+            <p className="changeturn-message">Your turn</p>
+        </div>
+    );
+}
 
 GameScreen.propTypes = {
     app: PropTypes.object.isRequired,
