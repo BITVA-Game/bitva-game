810dda3f5fe573a5d224e0006bd42774bbee9e36
MentalWombat <e.ochneva@gmail.com>
2019-03-18 17:06:00 +0300

    wip: show profile info

--- src/Profile.js
+++ src/Profile.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
+import MainMenu from './MainMenu';
 import './css/Profile.css';
 
 class Profile extends Component {
 	render() {
+		const characters = this.props.app.profile.characters.map(character => (
+			<span key={character}>{character}</span>
+		));
+		const deck = this.props.app.profile.deck.map(card => (
+			<span key={card}>{card}</span>
+		));
+
 		return(
-			<div className="profile">
-				Profile
+			<div className="profile app-background">
+				<div className="info">
+					<p>Characters: {characters}</p>
+					<p>Deck: {deck}</p>
+					<p>Gold: {this.props.app.profile.gold}</p>
+				</div>
+				<MainMenu sendMessage={this.props.sendMessage} />
 			</div>
 		);
 	}
