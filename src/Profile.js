c7b6df20d034925c146ba669ab55029084e390da
ML6503 <ml6503@gmail.com>
2019-03-18 16:52:05 +0200

    added test for Profile screen and deleted unneeded profile silver from tests + PropTypes added to Profile js

--- src/Profile.js
+++ src/Profile.js
@@ -1,27 +1,35 @@
+/* eslint-disable react/jsx-one-expression-per-line */
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import MainMenu from './MainMenu';
 import './css/Profile.css';
 
+// class created for future state use
+// eslint-disable-next-line react/prefer-stateless-function
 class Profile extends Component {
-	render() {
-		const characters = this.props.app.profile.characters.map(character => (
-			<span key={character}>{character}</span>
-		));
-		const deck = this.props.app.profile.deck.map(card => (
-			<span key={card}>{card}</span>
-		));
+    render() {
+        const characters = this.props.app.profile.characters.map(character => (
+            <span key={character}>{character}</span>
+        ));
+        const deck = this.props.app.profile.deck.map(card => (
+            <span key={card}>{card}</span>
+        ));
 
-		return(
-			<div className="profile app-background">
-				<div className="info">
-					<p>Characters: {characters}</p>
-					<p>Deck: {deck}</p>
-					<p>Gold: {this.props.app.profile.gold}</p>
-				</div>
-				<MainMenu sendMessage={this.props.sendMessage} />
-			</div>
-		);
-	}
+        return (
+            <div className="profile app-background">
+                <div className="info">
+                    <p>Characters: {characters}</p>
+                    <p>Deck: {deck}</p>
+                    <p>Gold: {this.props.app.profile.gold}</p>
+                </div>
+                <MainMenu sendMessage={this.props.sendMessage} />
+            </div>
+        );
+    }
 }
 
+Profile.propTypes = {
+    app: PropTypes.object.isRequired,
+    sendMessage: PropTypes.func.isRequired,
+};
 export default Profile;
