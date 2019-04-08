0e920d33593593e508ef78df6541f17936814f45
MentalWombat <e.ochneva@gmail.com>
2019-04-08 20:04:08 +0300

    wip

--- src/App.test.js
+++ src/App.test.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { render, fireEvent, cleanup } from 'react-testing-library';
 import 'jest-dom/extend-expect';
 import * as dom from 'dom-testing-library';
+import App from './App';
 
 
 afterEach(cleanup);
 
-test('renders without crashing', () => {
-    expect(true).toBeTruthy();
+test.only('renders without crashing', () => {
+    const { getByTestId, queryByTestId, queryAllByTestId } = render(<App/>);
+    expect(getByTestId("start-screen")).toBeTruthy();
 });
