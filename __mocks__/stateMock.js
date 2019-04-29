b1c4e0b409226e61839eb983c6937f5c5539ec51
MentalWombat <e.ochneva@gmail.com>
2019-04-29 14:40:37 +0300

    add state mocks for tests

---
+++ __mocks__/stateMock.js
@@ -0,0 +1,68 @@
+export const startscreenState = {
+    profile: {
+        characters: ['morevna', 'yaga', 'premudraya', 'hozyaika'],
+        deck: ['apple'],
+        gold: 0,
+    },
+    manager: {
+        screen: 'STARTSCREEN',
+    },
+    game: {
+
+    },
+};
+
+export const heroselectState = {
+    profile: {
+        characters: ['morevna', 'yaga', 'premudraya', 'hozyaika'],
+        deck: ['apple'],
+        gold: 0,
+    },
+    manager: {
+        screen: 'HEROSELECT',
+    },
+    game: {
+
+    },
+};
+
+export const heroselectedState = {
+    profile: {
+        characters: ['morevna', 'yaga', 'premudraya', 'hozyaika'],
+        deck: ['apple'],
+        gold: 0,
+    },
+    heroSelect: {
+    },
+    game: {
+
+    },
+};
+
+export const versusState = {
+    profile: {
+        characters: ['morevna', 'yaga', 'premudraya', 'hozyaika'],
+        deck: ['apple'],
+        gold: 0,
+    },
+    heroSelect: {
+    },
+    manager: {
+        screen: 'VERSUS',
+    },
+    game: {
+
+    },
+};
+
+export const profileState = {
+    profile: {
+        characters: ['morevna', 'yaga', 'premudraya', 'hozyaika'],
+        deck: ['apple'],
+        gold: 0,
+    },
+    manager: {
+        screen: 'PROFILE',
+    },
+    game: { phase: 'ACTIVE' },
+};
