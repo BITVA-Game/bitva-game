fad325c822e5fc1479ad315701ef9afc31ac12c1
ML6503 <ml6503@gmail.com>
2019-05-28 20:02:26 +0300

    test and function not to attack shield directly have been added

--- __tests__/simpleactions.test.js
+++ __tests__/simpleactions.test.js
@@ -1282,3 +1282,75 @@ test('msg ACTION received: no card in opponent item, player trying to attack, bu
     expect(result.game.players[0].health.current).toEqual(10);
     expect(result.game.players[1].health.current).toEqual(8);
 });
+
+// Test, that active player cannot attacks shield directly at item holder of opponent
+test.only('msg ACTION received: opponent has shield in item, player trying to attack, but cannot do it.', () => {
+    const msg = {
+        type: 'ACTION',
+        activeCard: 'key1',
+        target: 'itemOpponent',
+    };
+    // Mock sendReply function
+    const sendReply = jest.fn();
+    // Mock will rewrite all math.random and set active player card's key to key10
+    application.setApp({
+        game: {
+            phase: 'ACTIVE',
+            players: [
+                {
+                    active: true,
+                    hero: 'morevna',
+                    cards: {
+                        key0: {},
+                        key2: {},
+                        key17: {},
+                        key5: {},
+                        key7: {},
+                        key4: {},
+                        key6: {},
+                        key14: {},
+                        key12: {},
+                        key9: {},
+                    },
+                    health: { current: 10, maximum: 13 },
+                    hand: {
+                        key11: {},
+                        key8: {},
+                        key13: {},
+                        key1: {
+                            type: 'action', category: 'attack', points: 3, disabled: false,
+                        },
+                    },
+                    moveCounter: 1,
+                    item: {},
+                    grave: {},
+                },
+                {
+                    active: false,
+                    hero: 'yaga',
+                    health: { current: 8, maximum: 15 },
+                    item: {
+                        key10: {
+                            id: 'shieldSmall', type: 'item', category: 'shield', health: 2, healthCurrent: 2, disabled: false,
+                        },
+                    },
+                    grave: {},
+                },
+            ],
+        },
+    });
+    // Call the message function from application with this message and mocked function.
+    application.msgReceived(msg, sendReply);
+    expect(sendReply.mock.calls.length).toBe(1);
+
+    // to use it more easy let's save the received app into result
+    const result = sendReply.mock.calls[0][0];
+
+    // ожидаем, что карта dead water ушла на кладбище неактивного игрока
+    expect(Object.keys(result.game.players[0].hand)).toContain('key1');
+    // ожидаем, что moveCounter активного игрока не изменился
+    expect(result.game.players[0].moveCounter).toEqual(1);
+    // ожидаем, что текущее здоровье игроков не изменится
+    expect(result.game.players[0].health.current).toEqual(10);
+    expect(result.game.players[1].health.current).toEqual(8);
+});
