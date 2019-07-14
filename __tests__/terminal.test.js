951af44591ca91e8a005313401b7a1fd00ca84b8
Inga Pflaumer <inga.pflaumer@gmail.com>
2019-07-14 20:20:44 +1000

    ready for compleate test rewrite

---
+++ __tests__/terminal.test.js
@@ -0,0 +1,24 @@
+import {
+    startscreenState,
+} from '../__mocks__/stateMock';
+
+// import module for tests
+const application = require('../gameTerminal/application');
+
+jest.mock('../gameTerminal/randomFunc');
+
+// If it's the first INITIAL message from frontend, return the app in it's initial state
+test('Game loaded. Send the app in its initial state', () => {
+    // Create a messade that has type and may have additional data later.
+    // We only need type for this test.
+    const msg = { type: 'INIT' };
+
+    // Mock sendReply function
+    const sendReply = jest.fn();
+
+    // Call the message function from application with this message and mocked function.
+    application.msgReceived(msg, sendReply);
+
+    expect(sendReply.mock.calls.length).toBe(1);
+    expect(sendReply.mock.calls[0][0]).toMatchObject(startscreenState);
+});
