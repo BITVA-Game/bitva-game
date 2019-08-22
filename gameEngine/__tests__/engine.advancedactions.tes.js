3b5012af64d7bfa918887674cf51cb130de36080
ML6503 <ml6503@gmail.com>
2019-08-22 20:52:47 +0300

    test on living water in item act has been refactorred to engine.advancedtests

--- gameEngine/__tests__/engine.advancedactions.tes.js
+++ gameEngine/__tests__/engine.advancedactions.tes.js
@@ -18,7 +18,7 @@ const GameEngine = require('../index');
 
 jest.mock('../../gameTerminal/randomFunc');
 
-// Test, that  when living water is in item holder, then
+// Test, that  when player puts living water in item holder, then
 // players get +1 to their health current each until card has its points > 0
 test('msg ACTION received: active player put Living Water in item, it increases players health current for 1pnt.', () => {
     const cardToTest = 'key20';
@@ -99,6 +99,45 @@ test('msg ACTION received: active player has dead water in item, it decreases pl
     // помимо ущерба от очков карты атаки
     expect(inactivePlayer.health.current).toEqual(15);
     expect(activePlayer.health.current).toEqual(12);
+    // ожидаем, что карта-water находится в item пока у нее есть очки.
+    expect(inactivePlayer.item[cardToTest].points).not.toEqual(0);
+});
+
+// Test, that  when living water is in item holder, then
+// players get +1 to their current health each until card is in item holder
+test('msg ACTION received: active player put Living Water in item, it increases players health current for 1pnt.', () => {
+    const cardToTest = 'key24';
+    const wolfCard = 'key20';
+    const msg = {
+        type: message.ACTION,
+        activeCard: wolfCard,
+        target: target.OPPONENT,
+    };
+
+    const gameForTest = JSON.parse(JSON.stringify(dealAllState));
+    gameForTest.game.players[0].item.key24 = waterLiving;
+    gameForTest.game.players[0].hand.key20 = wolf;
+    gameForTest.game.players[0].health.current = 15;
+    gameForTest.game.players[1].health.current = 14;
+    gameForTest.game.players[0].moveCounter = 1;
+
+    const engine = new GameEngine(gameForTest);
+    engine.handle(msg);
+    const newGame = engine.getState();
+
+    // Find active and inactive players
+    const activePlayer = newGame.game.players.find(p => p.id === newGame.game.active);
+    const inactivePlayer = newGame.game.players.find(p => p.id !== newGame.game.active);
+    // ожидаем, что карта living water в item holder неактивного игрока
+    expect(Object.values(inactivePlayer.item).length).toEqual(1);
+    expect(inactivePlayer.item[cardToTest].id).toEqual(waterLiving.id);
+    // ожидаем, что карта  living water неактивного игрока имеет тип - heal
+    expect(inactivePlayer.item[cardToTest].category).toEqual(card.HEALCATEGORY);
+    // ожидаем, что очки карты living water неактивного игрока > 0
+    expect(inactivePlayer.item[cardToTest].points).toBeGreaterThan(0);
+    // ожидаем, что к текущему здоровью игроков прибавится по 1му очку
+    expect(inactivePlayer.health.current).toEqual(16);
+    expect(activePlayer.health.current).toEqual(13);
     // ожидаем, что карта-water находится в item пока у нее есть очки.
     expect(inactivePlayer.item[cardToTest].points).not.toEqual(0);
 });
