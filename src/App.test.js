1b93a07cf59e2b51d025691e987d35a55fcb9d15
MentalWombat <e.ochneva@gmail.com>
2019-08-25 14:40:53 +0300

    first test for startScreen and menu

--- src/App.test.js
+++ src/App.test.js
@@ -1,57 +1,32 @@
 import React from 'react';
-import { render, fireEvent, cleanup } from 'react-testing-library';
-import 'jest-dom/extend-expect';
-import * as dom from 'dom-testing-library';
+import { render, fireEvent, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 
 import electron from './electron-test';
 import App from './App';
-import StartScreen from './StartScreen';
-import MainMenu from './MainMenu';
 
 import { startscreenState } from '../gameTerminal/__data__/states';
 
 afterEach(cleanup);
 
-test.skip('renders without crashing', () => {
-    const { getByTestId } = render(<App />);
-    expect(getByTestId('app-screen')).toBeTruthy();
+test.only('renders startScreen and menu', () => {
+    const { getByTestId, getByAltText, getAllByTestId, getByText } = render(<App />);
     const stateSaver = electron.ipcRenderer.on.mock.calls[0][1];
-    stateSaver('eventStartSceen', startscreenState);
+    stateSaver('eventStartScreen', startscreenState);
 
-    const app = getByTestId('app-screen');
-    expect(app).toContainElement(getByTestId('start-screen'));
-    const startScreen = getByTestId('start-screen');
-    expect(startScreen).toContainElement(getByTestId('hollow-animation'));
-    expect(startScreen).toContainElement(getByTestId('window-animation'));
-    expect(startScreen).toContainElement(getByTestId('spider-animation'));
-    expect(startScreen).toContainElement(getByTestId('mushroom-animation'));
-    const menu = getByTestId('main-menu');
-    expect(startScreen).toContainElement(menu);
-});
+    expect(getByTestId('main-menu')).toBeTruthy();
+    expect(getByTestId('main-menu')).toHaveClass('sidebar-opened');
+    expect(getByTestId('close-menu-btn')).toBeTruthy();
 
-test.skip('<MainMenu> looks as expected', () => {
-    const stateSaver = electron.ipcRenderer.on.mock.calls[0][1];
-    stateSaver('eventStartSceen', startscreenState);
-    const { getByTestId, getAllByTestId } = render(<MainMenu />);
+    fireEvent.click(getByTestId('close-menu-btn'));
+    expect(getByTestId('main-menu')).not.toHaveClass('sidebar-opened');
+    expect(getByTestId('open-menu-btn')).toBeTruthy();
 
-    const mainMenu = getByTestId('main-menu');
-    expect(mainMenu).toContainElement(getByTestId('logo-container'));
-    expect(mainMenu).toContainElement(getByTestId('menu-buttons-container'));
-    const toggleBtn = getByTestId('toggle-btn');
-    expect(mainMenu).toContainElement(toggleBtn);
+    fireEvent.click(getByTestId('open-menu-btn'));
+    expect(getByTestId('main-menu')).toHaveClass('sidebar-opened');
+    expect(getByTestId('close-menu-btn')).toBeTruthy();
 
-    expect(getByTestId('menu-buttons-container')).toContainElement(getByTestId('toggle-button'));
-    const menuButtonsGroups = getAllByTestId('menu-buttons-group');
-    expect(menuButtonsGroups).toHaveLength(2);
-
-    const menuButtons = getAllByTestId('menu-button');
-    expect(menuButtons).toHaveLength(8);
-    expect(getByTestId('menu-buttons-group')).toContainElement(getByTestId('menu-button'));
-
-    let opened = true;
-
-    const toggle = jest.fn(() => opened = false);
-    fireEvent.click(getByTestId('toggle-button'), toggle());
-    expect(toggle).toHaveBeenCalledTimes(1);
-    expect(opened).toEqual(false);
+    expect(getByAltText('logo')).toBeTruthy();
+    expect(getAllByTestId('menu-button')).toHaveLength(8);
+    expect(getByText('Player vs Player')).toBeTruthy();
 });
