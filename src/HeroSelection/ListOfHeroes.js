87cb5f6807815494fa2d1ca892eb4848303a89b9
Inga Pflaumer <inga.pflaumer@gmail.com>
2019-09-22 15:08:33 +1000

    Hero  selection refactored before list addition

--- src/HeroSelection/ListOfHeroes.js
+++ src/HeroSelection/ListOfHeroes.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import './css/App.css';
-import './css/HeroSelection.css';
-import { sortedHeroesList } from './rules';
+import '../css/App.css';
+import '../css/HeroSelection.css';
 
-import yaga from './images/heroes/yaga_full.jpg';
-import morevna from './images/heroes/morevna_full.jpg';
-import hozyaika from './images/heroes/hozyaika_full.jpg';
-import premudraya from './images/heroes/premudraya_full.jpg';
-import heart from './images/icons/heart_red.png';
+import yaga from '../images/heroes/yaga_full.jpg';
+import morevna from '../images/heroes/morevna_full.jpg';
+import hozyaika from '../images/heroes/hozyaika_full.jpg';
+import premudraya from '../images/heroes/premudraya_full.jpg';
+import heart from '../images/icons/heart_red.png';
 
 const images = {
     yaga,
@@ -17,118 +16,63 @@ const images = {
     premudraya,
 };
 
-// footer section
-const Footer = props => (
-    <section className="heroselection-footer">
-        {props.hero
-            ? <BackButton onBack={props.onBack} />
-            : null
-        }
-        <Play selectHero={props.selectHero} tabIndex="5" />
-
+const Header = () => (
+    <section className="heroselection-header">
+        <div className="header-menu heroselection-title">
+            Select Character
+        </div>
     </section>
 );
 
-const Play = props => (
-    <div className="btn btn-play footer-menu" role="button" onClick={props.selectHero} onKeyPress={props.selectHero} tabIndex={props.tabIndex}>
-        PLAY
-    </div>
-);
-
-const BackButton = props => (
-    <div className="btn btn-back footer-menu" role="button" onClick={props.onBack} onKeyPress={props.onBack} tabIndex="10">
-        &#767;
-    </div>
-);
-
 // Individual hero block, repeates to display every character
-class HeroBlock extends React.Component {
-    highlighted() {
-        const highlight = this.props.selected ? 'btn-hero btn-hero-selected' : 'btn-hero icons-inactive';
-        return highlight;
-    }
-
-    render() {
-        const {
-            hero, isAvailable, changeSelected, onShow,
-        } = this.props;
-        return (
-            <div className={isAvailable ? 'hero-block' : 'hero-block hero-inaccessable'}>
-                <div className={this.highlighted()} role="button" onMouseEnter={() => changeSelected(hero.id)} onClick={onShow} onKeyPress={onShow} tabIndex="-1">
-                    <img className="heroselection-hero-image" src={images[hero.id]} alt={hero.id} />
-                    <div className="deck-icon">
-                        <div className="deck-text">
-                            {hero.cardsNumber}
-                        </div>
+const HeroBlock = (props) => {
+    const { hero, isAvailable, select } = props;
+    return (
+        <div className={isAvailable ? 'hero-block' : 'hero-block hero-inaccessable'}>
+            <div role="button" className="btn-hero" onClick={select} onKeyPress={select} tabIndex="-1">
+                <img className="heroselection-hero-image" src={images[hero.id]} alt={hero.id} />
+                <div className="deck-icon">
+                    <div className="deck-text">
+                        {hero.cardsNumber}
                     </div>
-                    <div className="health-container">
-                        <img className="health" src={heart} alt="" />
-                        <div className="health-text">
-                            {hero.health}
-                        </div>
+                </div>
+                <div className="health-container">
+                    <img className="health" src={heart} alt="" />
+                    <div className="health-text">
+                        {hero.health}
                     </div>
                 </div>
             </div>
-        );
-    }
-}
-
-// List of all characters, for each the HeroBlock is displayed.
-// Click will take the player into character info screen
-// opponent={props.opponent && hero.id === props.app.game.players[0].hero}
-const ListOfHeroes = (props) => {
-    const heroes = sortedHeroesList(props.app);
-    const characterIDs = props.app.heroSelect.heroes;
-
-    return (
-        <div>
-            <div className="heroes-list">
-                {heroes.map(hero => (
-                    <HeroBlock
-                        key={hero.id}
-                        onShow={props.onShow}
-                        hero={hero}
-                        app={props.app}
-                        isAvailable={characterIDs.includes(hero.id)}
-                        selected={hero.id === props.selected}
-                        changeSelected={props.changeSelected}
-                    />
-                ))}
-            </div>
-            {/*
-            <Footer
-                hero={this.state.hero}
-                selectHero={this.selectHero}
-                onBack={this.showHeroList}
-            />
-            */}
         </div>
     );
 };
 
+// List of all characters, for each the HeroBlock is displayed.
+// Click will take the player into character info screen
+// opponent={props.opponent && hero.id === props.app.game.players[0].hero}
+const ListOfHeroes = props => (
+    <div className="heroes-list">
+        {props.allHeroes.map(hero => (
+            <HeroBlock
+                key={hero.id}
+                select={() => props.select(hero.id)}
+                hero={hero}
+                isAvailable={props.heroesID.includes(hero.id)}
+            />
+        ))}
+    </div>
+);
+
 HeroBlock.propTypes = {
-    changeSelected: PropTypes.func.isRequired,
-    hero: PropTypes.object.isRequired,
-    onShow: PropTypes.func.isRequired,
-    selected: PropTypes.bool.isRequired,
     isAvailable: PropTypes.bool.isRequired,
+    select: PropTypes.func.isRequired,
+    hero: PropTypes.object.isRequired,
 };
 
 ListOfHeroes.propTypes = {
-    app: PropTypes.object.isRequired,
-    changeSelected: PropTypes.func.isRequired,
-    onShow: PropTypes.func.isRequired,
-    selected: PropTypes.string.isRequired,
-};
-
-Footer.propTypes = {
-    hero: PropTypes.object,
-    selectHero: PropTypes.func.isRequired,
-    onBack: PropTypes.func.isRequired,
-};
-
-Footer.defaultProps = {
-    hero: null,
+    heroesID: PropTypes.array.isRequired,
+    allHeroes: PropTypes.array.isRequired,
+    select: PropTypes.func.isRequired,
 };
 
 export default ListOfHeroes;
--- src/HeroSelection/index.js
+++ src/HeroSelection/index.js
@@ -4,8 +4,9 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import UIFx from 'uifx';
 import MainMenu from '../MainMenu';
-// import ListOfHeroes from './ListOfHeroes';
-// import OneHero from './OneHero';
+import HeroSwiper from './HeroSwiper';
+import ListOfHeroes from './ListOfHeroes';
+import OneHero from './OneHero';
 import '../css/App.css';
 import '../css/HeroSelection.css';
 import styles from '../css/HeroSelection.module.css';
@@ -16,14 +17,26 @@ import click2 from '../sound/fin.mp3';
 
 const clickSound2 = new UIFx(click2, { volume: 1.0 });
 
+
+const Header = props => (
+    <section className="heroselection-header">
+        <div className="header-menu heroselection-title">
+            {props.title}
+        </div>
+        {props.centre}
+    </section>
+);
+
 class HeroSelection extends Component {
     constructor(props) {
         super(props);
         this.app = props.app;
-        this.heroesList = sortedHeroesList(this.app).map(h => h.id);
         this.state = {
             hero: null,
         };
+        this.select = this.select.bind(this);
+        this.unselect = this.unselect.bind(this);
+        this.play = this.play.bind(this);
     }
 
     select(heroID) {
@@ -38,13 +51,44 @@ class HeroSelection extends Component {
     play() {
         console.log('PLAY MESSAGE SENT', this.state.hero);
         clickSound2.play();
-        this.props.sendMessage({ type: 'HEROSELECTED', hero: this.state.selected, player: this.app.heroSelect.activePlayer });
+        this.props.sendMessage({ type: 'HEROSELECTED', hero: this.state.hero.id, player: this.app.heroSelect.activePlayer });
+    }
+
+    heroesList() {
+        return Object.values(sortedHeroesList(this.app));
+    }
+
+    renderOneHero() {
+        const swiper = <HeroSwiper hero={this.state.hero} allHeroes={this.heroesList()} select={this.select} />;
+        return (
+            <div>
+                <Header title="Character Details" centre={swiper} />
+                <OneHero
+                    hero={this.state.hero}
+                    unselect={this.unselect}
+                    play={this.play}
+                />
+            </div>
+        );
+    }
+
+    renderListOfHeroes() {
+        return (
+            <div>
+                <Header title="Select Character" />
+                <ListOfHeroes
+                    heroesID={this.app.heroSelect.heroes}
+                    allHeroes={this.heroesList()}
+                    select={this.select}
+                />
+            </div>
+        );
     }
 
     render() {
         return (
             <div>
-                {/* this.state.hero ? <OneHero /> : <ListOfHeroes /> */}
+                {this.state.hero ? this.renderOneHero() : this.renderListOfHeroes() }
                 <MainMenu sendMessage={this.props.sendMessage} />
             </div>
         );
