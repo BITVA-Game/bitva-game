83395e77df3193ed8f1e9e18b9dc28dfa427ead3
ML6503 <ml6503@gmail.com>
2019-09-24 21:05:11 +0300

    clairvoyance 2nd test is added in refactoring

--- gameEngine/__tests__/engine.clairvoyance.test.js
+++ gameEngine/__tests__/engine.clairvoyance.test.js
@@ -9,7 +9,7 @@ import {
 import cards from '../__data__/cards';
 
 const {
-    clairvoyance,
+    clairvoyance, wolf,
 } = cards;
 
 const GameEngine = require('../index');
@@ -48,4 +48,37 @@ test('msg ACTION received: active player attacks with clairvoyance and can see n
     expect(Object.keys(inactivePlayer.cardsShown)).toContain('key1', 'key2', 'key4');
     // ожидаем, что у неактивного игрока, в cards сохранятся три карты показанные в cardsShown
     expect(Object.keys(inactivePlayer.cards)).toContain('key1', 'key2', 'key4');
+});
+
+// test to remove cardsShown from opponent object once player
+// attacked with clairvoyance card becomes active ( change of turn)
+test('msg ACTION received: when turn changes cardsShown is removed from player who has been attacked with clairvoyance card', () => {
+    // we define card key for testing
+    const cardToTest = 'key8';
+    const wolfCard = 'key21';
+    // we mock incoming message from frontend
+    const msg = {
+        type: message.ACTION,
+        activeCard: wolfCard,
+        target: target.OPPONENT,
+    };
+
+    // we put game engine into needed state
+    const gameForTest = JSON.parse(JSON.stringify(dealAllState));
+    gameForTest.game.players[0].grave.key8 = clairvoyance;
+    gameForTest.game.players[0].hand.key21 = wolf;
+   
+    // we create new engine with our game state
+    const engine = new GameEngine(gameForTest);
+    engine.handle(msg);
+    const newGame = engine.getState();
+
+    // We find active and inactive players
+    const activePlayer = newGame.game.players.find(p => p.id === newGame.game.active);
+    const inactivePlayer = newGame.game.players.find(p => p.id !== newGame.game.active);
+    // ожидаем, что Morevna ранее сходила картой clairvoyance, и та после ходя ушла на кладбище
+    expect(Object.keys(activePlayer.grave)).toContain(cardToTest);
+ 
+    // ожидаем, что у противника исчезнет cardsShown после 1 хода 
+    expect(Object.values(inactivePlayer)).not.toContain('cardsShown');
 });
