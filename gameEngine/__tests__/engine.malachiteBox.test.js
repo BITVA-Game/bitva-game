3ba3e154f954a86c82d499b3442c4dffed3ddb0c
ML6503 <ml6503@gmail.com>
2019-10-01 19:07:55 +0300

    malachite box backend tests refactoring done

---
+++ gameEngine/__tests__/engine.malachiteBox.test.js
@@ -0,0 +1,130 @@
+import {
+    dealAllState,
+} from '../__data__/states';
+
+import {
+    message, target, card,
+} from '../../constants';
+
+import cards from '../__data__/cards';
+
+const {
+    malachiteBox, bogatyr, apple,
+} = cards;
+
+const GameEngine = require('../index');
+
+jest.mock('../../gameTerminal/randomFunc');
+
+//* ****** Malachite Box Card Tests ** *//
+
+// test that once player attacks, while having malachiteBox card in item holder,
+// then opponent also is attacked by bat card
+test('msg ACTION received: active player has malachite box in item, when player acts then opponent is attacked by bat card', () => {
+    // we define card key for testing
+    const cardToTest = 'key24';
+    const bogatyrCard = 'key20';
+    // we mock incoming message from frontend
+    const msg = {
+        type: message.ACTION,
+        activeCard: bogatyrCard,
+        target: target.OPPONENT,
+    };
+
+    // we put game engine into needed state
+    const gameForTest = JSON.parse(JSON.stringify(dealAllState));
+    gameForTest.game.players[0].item.key24 = malachiteBox;
+    gameForTest.game.players[0].hand.key20 = bogatyr;
+
+    // we create new engine with our game state
+    const engine = new GameEngine(gameForTest);
+    engine.handle(msg);
+    const newGame = engine.getState();
+
+    // We find active and inactive players
+    const activePlayer = newGame.game.players.find((p) => p.id === newGame.game.active);
+    const inactivePlayer = newGame.game.players.find((p) => p.id !== newGame.game.active);
+
+    // ожидаем, что у активного игрока в item holder лежит карта malachiteBox
+    expect(Object.values(activePlayer.item).length).toEqual(1);
+    expect(activePlayer.item[cardToTest].id).toEqual(malachiteBox.id);
+    expect(activePlayer.item[cardToTest].type).toEqual(card.ITEMCATEGORY);
+    expect(Object.keys(activePlayer.grave)).not.toContain(cardToTest);
+    // ожидаем, что очки карты malachiteBox  > 0
+    expect(activePlayer.item[cardToTest].health).toBeGreaterThan(0);
+    // ожидаем, что от текущего здоровья активного игрока убавятся:
+    // 4 очка от атаки картой богатырь и
+    // 1 очко от атаки картой летучая мышь ( показана анимацией)
+    expect(inactivePlayer.health.current).toEqual(10);
+});
+
+// test that once player heal herself, while having malachiteBox card in item holder,
+// then opponent also is attacked by bat card
+test('msg ACTION received: active player has malachite box in item, when player heal then opponent is attacked by bat card', () => {
+    // we define card key for testing
+    const cardToTest = 'key24';
+    const appleCard = 'key20';
+    // we mock incoming message from frontend
+    const msg = {
+        type: message.ACTION,
+        activeCard: appleCard,
+        target: target.HERO,
+    };
+
+    // we put game engine into needed state
+    const gameForTest = JSON.parse(JSON.stringify(dealAllState));
+    gameForTest.game.players[0].item.key24 = malachiteBox;
+    gameForTest.game.players[0].hand.key20 = apple;
+    gameForTest.game.players[0].health.current = 10;
+    // we create new engine with our game state
+    const engine = new GameEngine(gameForTest);
+    engine.handle(msg);
+    const newGame = engine.getState();
+
+    // We find active and inactive players
+    const activePlayer = newGame.game.players.find((p) => p.id === newGame.game.active);
+    const inactivePlayer = newGame.game.players.find((p) => p.id !== newGame.game.active);
+
+    // ожидаем, что у активного игрока в item holder лежит карта malachiteBox
+    expect(activePlayer.item[cardToTest].id).toEqual(malachiteBox.id);
+    expect(Object.keys(activePlayer.grave)).not.toContain(cardToTest);
+    // ожидаем, что от текущего здоровья оппонента - неактивного игрока
+    // отнимется 1 очко от атаки картой летучая мышь ( показана анимацией)
+    expect(inactivePlayer.health.current).toEqual(14);
+    // ожидаем, что активный поправит свое здоровье на 2 очка карты яблоко
+    expect(activePlayer.health.current).toEqual(12);
+});
+
+// test that active player can put malachiteBox card in item holder,
+// and opponent health current does not increased immediately (only on next act of player)
+test('msg ACTION received: player can put malachiteBox in item, opponent health is not increased', () => {
+    // we define card key for testing
+    const cardToTest = 'key20';
+    // we mock incoming message from frontend
+    const msg = {
+        type: message.ACTION,
+        activeCard: cardToTest,
+        target: target.ITEMCARD,
+    };
+
+    // we put game engine into needed state
+    const gameForTest = JSON.parse(JSON.stringify(dealAllState));
+    gameForTest.game.players[0].hand.key20 = malachiteBox;
+
+
+    // we create new engine with our game state
+    const engine = new GameEngine(gameForTest);
+    engine.handle(msg);
+    const newGame = engine.getState();
+
+    // We find active and inactive players
+    const activePlayer = newGame.game.players.find((p) => p.id === newGame.game.active);
+    const inactivePlayer = newGame.game.players.find((p) => p.id !== newGame.game.active);
+
+    // ожидаем, что у активного игрока в item holder лежит карта malachiteBox
+    expect(Object.values(activePlayer.item).length).toEqual(1);
+    expect(activePlayer.item[cardToTest].id).toEqual(malachiteBox.id);
+    expect(Object.keys(activePlayer.grave)).not.toContain(cardToTest);
+    // ожидаем, что у неактивного игрока не изменится, health current == 15
+    expect(inactivePlayer.health.current).toEqual(15);
+});
