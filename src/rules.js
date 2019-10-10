c36a0016f2e912572e42d9d19fca4657448e4199
Inga Pflaumer <inga.pflaumer@gmail.com>
2019-10-10 19:21:15 +1100

    I changed the rules with no reason

--- src/rules.js
+++ src/rules.js
@@ -3,27 +3,27 @@ export default function isTarget(target, dragging, active, player) {
         return false;
     }
     if (!active) {
-        // console.log(player.item);
+    // console.log(player.item);
         const itemKey = Object.keys(player.item)[0];
         // eslint-disable-next-line no-unused-expressions
         itemKey ? player.item[itemKey].category : null;
         // console.log(itemCategory);
         return (
             (target === 'opponent' && dragging.card.category === 'attack')
-            || (target === 'itemOpponent' && dragging.card.category === 'attack')
-            || (target === 'opponent' && dragging.card.category === 'holdCard')
-            || (target === 'opponent' && dragging.card.category === 'attackItems')
-            || (target === 'opponent' && dragging.card.category === 'turning')
-            || (target === 'opponent' && dragging.card.category === 'showCards')
+      || (target === 'itemOpponent' && dragging.card.category === 'attack')
+      || (target === 'opponent' && dragging.card.category === 'holdCard')
+      || (target === 'opponent' && dragging.card.category === 'attackItems')
+      || (target === 'opponent' && dragging.card.category === 'turning')
+      || (target === 'opponent' && dragging.card.category === 'showCards')
+      || (target === 'opponent' && dragging.card.category === 'noCards')
         );
     }
     return (
         (target === 'hero' && dragging.card.category === 'heal')
-            || (target === 'item' && dragging.card.type === 'item')
-            || (target === 'graveyard')
+    || (target === 'item' && dragging.card.type === 'item')
+    || target === 'graveyard'
     );
 }
-
 
 export function getActiveProfile(app) {
     return app.profiles.find((p) => p.id === app.game.active);
