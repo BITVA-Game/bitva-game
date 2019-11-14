d2f0a54a9a8f905b48bbe5dc14dff920218a784b
ML6503 <ml6503@gmail.com>
2019-11-14 22:29:35 +0200

    eslint error fixed

--- gameEngine/__tests__/engine.mushrooms.test.js
+++ gameEngine/__tests__/engine.mushrooms.test.js
@@ -4,7 +4,7 @@ import {
 } from '../__data__/states';
 
 import {
-    message, target, card,
+    message, target,
 } from '../../constants';
 
 import cards from '../__data__/cards';
@@ -21,7 +21,8 @@ jest.mock('../../gameTerminal/randomFunc');
 
 // Test, that active player can put Forest Mushrooms card in item holder
 // then once turn changes, at next action opponent can get forest mushroom panic with 60 % chance,
-// so opponent at the beggining of his next action can act only with this one random card from his/her hand ( move counter +1)
+// so opponent at the beggining of his next action can act only
+// with this one random card from his/her hand ( move counter +1)
 test('EDGE CASE TEST once player put forestMushroom card as item, then with 60% opponent can get only 1 random card available from hand.', () => {
     // we define card key for testing
     const cardToTest = 'key20';
@@ -68,8 +69,9 @@ test('EDGE CASE TEST once player put forestMushroom card as item, then with 60% 
     // ожидаем, что карта forest mushrooms в item holder неактивного игрока Yaga
     // так как произошла смена хода
     expect(Object.keys(inactivePlayer.item)).toContain(cardToTest);
-    // ожидаем, так как сменился ход - Morevna стала активной, и с 60% вероятностью лесные грибы сработали
-    // и одна рэндомная карта в руке Morevna приобрела свойство panic: false, остальные - panic: true
+    // ожидаем, так как сменился ход - Morevna стала активной,
+    // с 60% вероятностью лесные грибы сработали и одна рэндомная карта в руке Morevna
+    // приобрела свойство panic: false, остальные - panic: true
     expect(activePlayer.hand).toEqual(
         {
             key10: {
@@ -93,9 +95,12 @@ test('EDGE CASE TEST once player put forestMushroom card as item, then with 60% 
 });
 
 // Test that if opponent has forestMushroom card in item holder, then with 60 % chance,
-// active player's one random card in hand got panic: false property and other cards - panic: true incl item, at the beggining of action
-// and then player acted with this one random card, it went to graveyard and panic: false property dissapeared
-// and then again with 60 % chance cards got panic: true property except one random that gets panic: false
+// active player's one random card in hand got panic: false property
+// and other cards - panic: true incl item, at the beggining of action
+// and then player acted with this one random card,
+// it went to graveyard and panic: false property dissapeared
+// and then again with 60 % chance cards got panic: true property
+// except one random that gets panic: false
 test('EDGE CASE TEST if forestMushroom card is at opponent item, then with 60% player can act only by 1 random card from hand at this action.', () => {
     // we define card key for testing
     const cardToTest = 'key20';
@@ -170,7 +175,7 @@ test('EDGE CASE TEST if forestMushroom card is at opponent item, then with 60% p
 
 // Test that if opponent has forestMushroom card in item holder, and with 60 % chance,
 // active player's one random card in hand got at the beggining of action panic: false property ,
-// and now player acted with this one random card, it went to graveyard and panic property dissapeared
+// player acted with this one random card, it went to graveyard and panic property dissapeared
 // 'forestMushroom' card has lost its healthCurrent points and went to graveyard
 // no cards in hand of player got any panic property now
 test('EDGE CASE TEST if by forestMushroom card player got 60% chance and acted by 1 random card, now mushroom went to gravyard and no panic property.', () => {
@@ -229,8 +234,8 @@ test('EDGE CASE TEST if by forestMushroom card player got 60% chance and acted b
     );
 });
 
-// Test that if active player has forestMushroom card in item holder from previous turn, then with 60 % chance,
-// opponent's one random card in hand got at previous act panic: true property ,
+// Test that if active player has forestMushroom card in item from previous turn,
+// with 60 % chance, opponent's one random card in hand got at previous act panic: true property ,
 // then if active player acts now - none of the cards get panic property
 test('EDGE CASE TEST if forestMushroom card is at opponent item, player got 60% chance and acted with 1 random card from hand, turn change, no panic .', () => {
     // we define card key for testing
@@ -298,7 +303,8 @@ test('EDGE CASE TEST if forestMushroom card is at opponent item, player got 60% 
 // Test that if opponent atacked player with russianOven,
 // player got 2 cards with disabled: true ( he can not act with these cards)
 // and since opponent has forestMushroom card in item holder, then with 60 % chance,
-// active player's one random card in hand got panic: false property and other cards - panic: true, at the beggining of action
+// active player's one random card in hand got panic: false property
+// other cards - panic: true, at the beggining of action
 // and if  the same random card get panic: false that already got disabled: true,
 // then we delete disbabled: true and override panice: false instead - player can act with this card
 test('EDGE CASE TEST forestMushroom card is at opponent item && player attacked by russianOver, then with 60% player can act only by 1 random card even if it was chained by russian Oven.', () => {
@@ -331,7 +337,7 @@ test('EDGE CASE TEST forestMushroom card is at opponent item && player attacked 
             id: 'chemise', type: 'action', category: 'heal', points: 5, initialpoints: 5, disabled: true,
         },
     };
-    
+
     // we create new engine with our game state
     const engine = new GameEngine(gameForTest);
     engine.handle(msg);
@@ -343,7 +349,7 @@ test('EDGE CASE TEST forestMushroom card is at opponent item && player attacked 
 
     // ожидаем, что карта forest mushrooms в item holder неактивного игрока Yaga
     expect(Object.keys(inactivePlayer.item)).toContain(cardToTest);
-    // ожидаем, что оставшиеся карта в руке Morevna потеряла свойство disabled: true и получила panic: false
+    // ожидаем, что карта в руке Morevna потеряла свойство disabled: true и получила panic: false
     expect(activePlayer.hand).toEqual(
         {
             key10: {
