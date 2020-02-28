f0711ac7dc35415a93603e4874c4f33f2f64fd9b
ML6503 <ml6503@gmail.com>
2020-02-28 22:42:54 +0200

    we call animation now with setTimeout instead of setInterval

--- src/GameScreen/AnimationBirds.js
+++ src/GameScreen/AnimationBirds.js
@@ -1,19 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import playSound from '../soundController';
 
-const Birds = () => (
-    <div className="animation-game-screen">
-        <div className="bird-container bird-container-one">
-            <div className="bird bird-one" />
+const Birds = () => {
+    playSound('birds');
+    return (
+        <div className="animation-game-screen">
+            <div className="bird-container bird-container-one">
+                <div className="bird bird-one" />
+            </div>
+            <div className="bird-container bird-container-two">
+                <div className="bird bird-two" />
+            </div>
+            <div className="bird-container bird-container-three">
+                <div className="bird bird-three" />
+            </div>
         </div>
-        <div className="bird-container bird-container-two">
-            <div className="bird bird-two" />
-        </div>
-        <div className="bird-container bird-container-three">
-            <div className="bird bird-three" />
-        </div>
-    </div>
-);
+    );
+};
 
 const BirdsAnimation = () => {
     // animation duration time
@@ -21,31 +24,27 @@ const BirdsAnimation = () => {
     const [birdsAnim, setBirdsAnim] = useState(false);
 
     // min time delay to start animation
-    const minStart = 60000;
+    // const minStart = 60000;
+    const minStart = 20000;
     // max time delay to start animation
-    const maxStart = 180000;
+    // const maxStart = 180000;
+    const maxStart = 60000;
     // random time delay to start animation between mmin and max values
     const animationStart = Math.floor(Math.random() * ((maxStart - minStart) + 1)) + minStart;
 
     // we change state after 1st render - didMount and birdsAnim === true to return BIRDS
     // after we call random delay there will be another re-render and we make birdsAnim = false
     // then we return birdsAnim == true with random delay
-    const startBirds = () => setInterval(() => {
-        playSound('birds');
-        setBirdsAnim(true);
-    }, animationDuration + animationStart);
-
     useEffect(() => {
-        startBirds();
-        if (birdsAnim === true) {
-            clearInterval(startBirds);
+        if (birdsAnim === false) {
+            setTimeout(() => {
+                setBirdsAnim(true);
+            }, animationStart);
+        } if (birdsAnim === true) {
             setTimeout(() => {
                 setBirdsAnim(false);
             }, animationDuration);
         }
-        return () => {
-            clearInterval(startBirds);
-        };
     }, [birdsAnim]);
 
     const birdsAnimComp = birdsAnim
--- src/GameScreen/index.js
+++ src/GameScreen/index.js
@@ -8,7 +8,7 @@ import '../css/GameScreen.css';
 import { getActivePlayer, getInActivePlayer } from '../rules';
 import { withBoardContext } from './BoardContext';
 import playSound from '../soundController';
-import BirdsAnimation from './AnimationController';
+import BirdsAnimation from './AnimationBirds';
 
 class GameScreen extends Component {
     constructor(props) {
