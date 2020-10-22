d1c03fdde3b39918d403171d4a1a295e192a6b9d
ML6503 <ml6503@gmail.com>
2020-10-22 19:40:48 +0300

    NewLogin refactoring

--- src/LoginScreen/NewLogin.js
+++ src/LoginScreen/NewLogin.js
@@ -1,51 +1,46 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import faker from 'faker';
 
 const { message } = require('../constants');
 
-class NewLogin extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: faker.name.findName(),
-        };
-        this.inputChange = this.inputChange.bind(this);
-        this.createAccount = this.createAccount.bind(this);
-    }
+const NewLogin = (props) => {
+    const [value, setValue] = useState(faker.name.findName());
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {
+    //         value: faker.name.findName(),
+    //     };
+    //     this.inputChange = this.inputChange.bind(this);
+    //     this.createAccount = this.createAccount.bind(this);
+    // }
 
-
-    createAccount(event) {
+    const createAccount = (event) => {
         event.preventDefault();
-        this.props.sendMessage({
+        props.sendMessage({
             type: message.CREATEACC,
-            account: this.state.value,
+            account: value,
         });
-        this.props.toggleForm();
-    }
+        props.toggleForm();
+    };
+    const inputChange = (event) => setValue(event.target.value);
 
-    inputChange(event) {
-        this.setState({ value: event.target.value });
-    }
-
-    render() {
-        return (
-            <div className="login-new-form">
-                <h3>Enter Account Name</h3>
-                <form onSubmit={this.createAccount}>
-                    <div className="account-name-input">
-                        <label htmlFor="name">Name</label>
-                        <input type="text" name="name" value={this.state.value} onChange={this.inputChange} />
-                    </div>
-                    <div className="login-buttons">
-                        <input className="login-button" type="button" value="Cancel" onClick={() => this.props.toggleForm()} />
-                        <input className="login-button" type="submit" value="Create" />
-                    </div>
-                </form>
-            </div>
-        );
-    }
-}
+    return (
+        <div className="login-new-form">
+            <h3>Enter Account Name</h3>
+            <form onSubmit={createAccount}>
+                <div className="account-name-input">
+                    <label htmlFor="name">Name</label>
+                    <input type="text" name="name" value={value} onChange={inputChange} />
+                </div>
+                <div className="login-buttons">
+                    <input className="login-button" type="button" value="Cancel" onClick={() => props.toggleForm()} />
+                    <input className="login-button" type="submit" value="Create" />
+                </div>
+            </form>
+        </div>
+    );
+};
 
 NewLogin.propTypes = {
     sendMessage: PropTypes.func.isRequired,
