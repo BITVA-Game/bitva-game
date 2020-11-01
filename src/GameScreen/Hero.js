8749314d3a01096c117e09a609f56f36b5c8fd58
ML6503 <ml6503@gmail.com>
2020-11-01 15:01:43 +0200

    Hero refactoring

--- src/GameScreen/Hero.js
+++ src/GameScreen/Hero.js
@@ -1,8 +1,6 @@
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
-// we disable curly-newline as Code Climate gives error if on new line
-// eslint-disable-next-line object-curly-newline
-import React, { Component, useContext, useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import BoardContext from './BoardContext';
 
@@ -25,7 +23,6 @@ const images = {
     hozyaika,
     premudraya,
 };
-
 const HeroImage = ({ hero }) => (
     <img
         className="hero-image"
@@ -35,8 +32,6 @@ const HeroImage = ({ hero }) => (
     />
 
 );
-
-
 const Hero = (props) => {
     const { isTarget, cardDropped, cardOver } = useContext(BoardContext);
     const activeHero = props.active ? target.HERO : target.OPPONENT;
@@ -91,74 +86,68 @@ const Hero = (props) => {
     );
 };
 
-class HealthMeter extends Component {
-    constructor(props) {
-        super(props);
-        // get the coordinates of the healthmeter center (half of the width and height)
-        this.cx = props.width / 2;
-        this.cy = props.height / 2;
-        // set stroke width of the healthmeter circle
-        this.sw = 6;
-        // get the radius of the healthmeter circle
-        this.r = this.cx - this.sw / 2;
-        // get the circle length
-        const cl = 2 * Math.PI * this.r;
+const HealthMeter = (props) => {
+    // get the coordinates of the healthmeter center (half of the width and height)
+    const cx = props.width / 2;
+    const cy = props.height / 2;
+    // set stroke width of the healthmeter circle
+    const sw = 6;
+    // get the radius of the healthmeter circle
+    const r = cx - sw / 2;
+    // get the circle length
+    const cl = 2 * Math.PI * r;
+    // set maximum level of the hero health
+    const maximum = props.health.maximum;
+    // get an angle of the current health level
+    const angle = (360 / maximum) * props.health.current - 90;
+    // check if the current health level is greater than a half
+    // than an elliptic ark is long and goes along a large curve
+    // it's a large-arc-flag in w3.org terminology
+    // <https://www.w3.org/TR/SVG/paths.html#PathDataEllipticalArcCommands>
+    // we add 90 degrees cause originally svg circle starts from the left
+    // through the top to the right but we need it starts from the top
+    const long = angle + 90 > 180 ? 1 : 0;
+    // get the coordinates of the end of the elliptic arc
+    const x = cx + r * Math.cos((angle * Math.PI - 1) / 180);
+    const y = cy + r * Math.sin((angle * Math.PI - 1) / 180);
 
-        // set maximum level of the hero health
-        const maximum = props.health.maximum;
-        // get an angle of the current health level
-        const angle = (360 / maximum) * props.health.current - 90;
-        // check if the current health level is greater than a half
-        // than an elliptic ark is long and goes along a large curve
-        // it's a large-arc-flag in w3.org terminology
-        // <https://www.w3.org/TR/SVG/paths.html#PathDataEllipticalArcCommands>
-        // we add 90 degrees cause originally svg circle starts from the left
-        // through the top to the right but we need it starts from the top
-        const long = angle + 90 > 180 ? 1 : 0;
-        // get the coordinates of the end of the elliptic arc
-        const x = this.cx + this.r * Math.cos((angle * Math.PI - 1) / 180);
-        const y = this.cy + this.r * Math.sin((angle * Math.PI - 1) / 180);
+    // set the svg path: from the left-top corner M(oveto) the right on half of the healthmeter
+    // width and to the down on half of the stroke width, then draw the elliptic A(rc) with
+    // these parameters: radii rx and ry x-axis-rotation large-arc-flag sweep-flag
+    // to the x y coordinates <https://www.w3.org/TR/SVG/images/paths/arcs02.png>
+    const d = `M ${cx} ${sw / 2} A ${r} ${r} 0 ${long} 1 ${x} ${y}`;
+    // get the strokeDasharray parameter for the path
+    // (the length of the one health point and 1 for the gap)
+    const sda = `${cl / maximum - 1},1`;
+    // get the strokeDashoffset parameter to start the path with indentation (not used now)
+    // const sdo = (cl / 360) * 90;
 
-        // set the svg path: from the left-top corner M(oveto) the right on half of the healthmeter
-        // width and to the down on half of the stroke width, then draw the elliptic A(rc) with
-        // these parameters: radii rx and ry x-axis-rotation large-arc-flag sweep-flag
-        // to the x y coordinates <https://www.w3.org/TR/SVG/images/paths/arcs02.png>
-        this.d = `M ${this.cx} ${this.sw / 2} A ${this.r} ${this.r} 0 ${long} 1 ${x} ${y}`;
-        // get the strokeDasharray parameter for the path
-        // (the length of the one health point and 1 for the gap)
-        this.sda = `${cl / maximum - 1},1`;
-        // get the strokeDashoffset parameter to start the path with indentation (not used now)
-        this.sdo = (cl / 360) * 90;
-    }
-
-    render() {
-        return (
-            <>
-                <svg className="hero-health" width={this.cx * 2} height={this.cy * 2}>
-                    <circle
-                        cx={this.cx}
-                        cy={this.cy}
-                        r={this.r}
-                        stroke="black"
-                        strokeWidth={this.sw + 1}
-                        fill="transparent"
-                    />
-                    <path
-                        className="hero-health-points"
-                        d={this.d}
-                        strokeWidth={this.sw}
-                        strokeDasharray={this.sda}
-                        strokeLinecap="butt"
-                        fill="transparent"
-                    />
-                </svg>
-                <div className="hero-icon icon-heart">
-                    <span className="game-icon-text">{this.props.health.current}</span>
-                </div>
-            </>
-        );
-    }
-}
+    return (
+        <>
+            <svg className="hero-health" width={cx * 2} height={cy * 2}>
+                <circle
+                    cx={cx}
+                    cy={cy}
+                    r={r}
+                    stroke="black"
+                    strokeWidth={sw + 1}
+                    fill="transparent"
+                />
+                <path
+                    className="hero-health-points"
+                    d={d}
+                    strokeWidth={sw}
+                    strokeDasharray={sda}
+                    strokeLinecap="butt"
+                    fill="transparent"
+                />
+            </svg>
+            <div className="hero-icon icon-heart">
+                <span className="game-icon-text">{props.health.current}</span>
+            </div>
+        </>
+    );
+};
 
 Hero.propTypes = {
     player: PropTypes.object.isRequired,
