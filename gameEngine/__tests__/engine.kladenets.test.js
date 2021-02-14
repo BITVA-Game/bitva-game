5703db851dfc99e7248f91d51c1d4e3c77f9587c
ML6503 <ml6503@gmail.com>
2021-02-14 13:29:48 +0200

    new test added to put kladenets into item holder + added to characters.json to test

--- gameEngine/__tests__/engine.kladenets.test.js
+++ gameEngine/__tests__/engine.kladenets.test.js
@@ -19,7 +19,7 @@ jest.mock('../../gameTerminal/randomFunc');
 //* ****** Kladenets Card Tests ** *//
 
 // Test, that when Kladenets is in active player item holder
-// then once actove player attacks his attack points are increased by 1p
+// then once active player attacks his attack points are increased by 1p
 
 test('msg ACTION received: active player has kladenetsin item, it increase all attacks against opponent for 1pnt.', () => {
     // we define card key for testing
@@ -100,6 +100,42 @@ test('msg ACTION received: inactive player has kladenets in item, it attacked wi
     expect(inactivePlayer.health.current).toEqual(15);
     // ожидаем, что от текущего здоровья активного игрока не изменится
     expect(activePlayer.health.current).toEqual(16);
+    // ожидаем, что  фаза игры не сменится с ACTIVE
+    expect(newGame.game.phase).toEqual('ACTIVE');
+});
+
+// Test, that active player can put Kladenets into item holder
+test('msg ACTION received: active player put kladenets into item holder', () => {
+    // we define card key for testing
+    const cardToTest = 'key24';
+    // we mock incoming message from frontend
+    const msg = {
+        type: message.ACTION,
+        activeCard: cardToTest,
+        target: target.ITEMCARD,
+    };
+
+    // we put game engine into needed state
+    const gameForTest = JSON.parse(JSON.stringify(dealAllState));
+    gameForTest.game.players[0].hand.key24 = kladenets;
+
+    // we create new engine with our game state
+    const engine = new GameEngine(gameForTest);
+    engine.handle(msg);
+    const newGame = engine.getState();
+
+    // We find active and inactive players
+    const activePlayer = newGame.game.players.find((p) => p.id === newGame.game.active);
+
+    // ожидаем, что карта kladenets в item holder активного игрока,
+    expect(Object.values(activePlayer.item).length).toEqual(1);
+    expect(activePlayer.item[cardToTest].id).toEqual(kladenets.id);
+    expect(activePlayer.item[cardToTest].type).toEqual(card.ITEMCATEGORY);
+
+    // ожидаем, что карта kladenets имеет category - attack
+    expect(activePlayer.item[cardToTest].category).toEqual(card.ATTACKCATEGORY);
+    // ожидаем, что очки карты kladenets  > 0
+    expect(activePlayer.item[cardToTest].health).toBeGreaterThan(0);
     // ожидаем, что  фаза игры не сменится с ACTIVE
     expect(newGame.game.phase).toEqual('ACTIVE');
 });
